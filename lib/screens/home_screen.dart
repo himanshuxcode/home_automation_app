bcdaad653c9c4033b3ae19b34b437873cafb6f1e
Himanshu <himanshuxcode@gmail.com>
2024-11-05 08:44:59 +0530

    Images Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:has_flutter/screens/room_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,45 +17,83 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: const Color(0xFFB7E0FF),
         title: Text("My Home"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications_outlined),
+          ),
+        ],
       ),
+      drawer: Drawer(),
       body: Padding(
         padding: const EdgeInsets.symmetric(
-        vertical: 10.0,
+          vertical: 10.0,
           horizontal: 20.0,
         ),
         child: GridView(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             crossAxisSpacing: 12.0,
-            mainAxisSpacing: 12.0,
           ),
           children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
+            _roomCard("Living room", "assets/images/living.jpg"),
+            _roomCard("Kitchen", "assets/images/kitchen.jpg"),
+            _roomCard("Bedroom 1", "assets/images/bedroom-1.jpg"),
+            _roomCard("Bedroom 2", "assets/images/bedroom-2.jpg"),
+            _roomCard("Washroom", "assets/images/washroom.jpg"),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _roomCard(
+    String label,
+    String img,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RoomScreen(),
+          ),
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(10.0),
+              ),
+              image: DecorationImage(
+                image: AssetImage(
+                  img,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 30.0,
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(10.0),
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
